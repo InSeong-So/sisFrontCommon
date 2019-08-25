0f9b6101a663d34ccfaeb2bc0afb1b2d0d49cc78
InSeong-So <goflvhxj2547@gmail.com>
2019-08-25 21:07:18 +0900

    파일 다운로드 서블릿 구현중

--- drillCode/soloWeb3/src/core/util/DownloadAction.java
+++ drillCode/soloWeb3/src/core/util/DownloadAction.java
@@ -1,8 +1,13 @@
 package core.util;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,12 +34,34 @@ public class DownloadAction extends HttpServlet
     {
         sisAction(request, response);
     }
-
-    private void sisAction(HttpServletRequest request, HttpServletResponse response)
-    {
-        // TODO Auto-generated method stub
-        
-    }
     
+    private void sisAction(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        String filename = "";
+        String originFilename = "";
+        File file = new File(filename);
+        int fileLengh = 0;
+        ServletOutputStream op = response.getOutputStream();
+        ServletContext context = getServletConfig().getServletContext();
+        String mimeType = context.getMimeType(filename);
+        response.setContentType((mimeType != null) ? mimeType : "application/octet-stream");
+        response.setContentLength((int) file.length());
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + originFilename + "\"");
+        
+        //
+        // Stream to the requester.
+        //
+        byte[] bbuf = new byte[100000];
+        DataInputStream in = new DataInputStream(new FileInputStream(file));
+        
+        while ((in != null) && ((fileLengh = in.read(bbuf)) != -1))
+        {
+            op.write(bbuf, 0, fileLengh);
+        }
+        
+        in.close();
+        op.flush();
+        op.close();
+    }
     
 }
